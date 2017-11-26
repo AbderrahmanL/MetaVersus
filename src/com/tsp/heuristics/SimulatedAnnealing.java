6f9e7a22fa8eb08f3e41f779b920937e55735e8f
Abdo LAHI <lahiaouni.abd@gmail.com>
2017-11-26 17:58:07 +0000

    Minor changes

--- src/com/tsp/heuristics/SimulatedAnnealing.java
+++ src/com/tsp/heuristics/SimulatedAnnealing.java
@@ -5,8 +5,35 @@ import com.tsp.shared.Tour;
 import com.tsp.shared.Utility;
 import com.tsp.shared.TourManager;
 
+import java.util.Random;
+
 public class SimulatedAnnealing {
 
+    /**
+     * Calculates the acceptance probability
+     */
+    public static double acceptanceProbability(int currentDistance, int newDistance, double temperature) {
+        // If the new solution is better, accept it
+        if (newDistance < currentDistance) {
+            return 1.0;
+        }
+        // If the new solution is worse, calculate an acceptance probability
+        return Math.exp((currentDistance - newDistance) / temperature);
+    }
+
+    /**
+     * this method returns a random number n such that
+     * 0.0 <= n <= 1.0
+     */
+    public static double randomDouble()
+    {
+        Random r = new Random();
+        return r.nextInt(1000) / 1000.0;
+    }
+
+    /*
+    * The heuristic main algorithm implementation
+    * */
     public static void startHeuristic() {
 
 
@@ -54,8 +81,8 @@ public class SimulatedAnnealing {
                 int neighbourDistance = newSolution.getTotalDistance();
 
                 // Decide if we should accept the neighbour
-                double rand = Utility.randomDouble();
-                if (Utility.acceptanceProbability(currentDistance, neighbourDistance, temp) > rand) {
+                double rand = randomDouble();
+                if (acceptanceProbability(currentDistance, neighbourDistance, temp) > rand) {
                     currentSolution = new Tour(newSolution.getTour());
                 }
 
--- src/com/tsp/shared/Utility.java
+++ src/com/tsp/shared/Utility.java
@@ -39,28 +39,6 @@ public class Utility {
 			e.printStackTrace();
 		}
 	}
-		
-	/**
-	 * Calculates the acceptance probability
-	 */
-	public static double acceptanceProbability(int currentDistance, int newDistance, double temperature) {
-		// If the new solution is better, accept it
-		if (newDistance < currentDistance) {
-			return 1.0;
-		}
-		// If the new solution is worse, calculate an acceptance probability
-		return Math.exp((currentDistance - newDistance) / temperature);
-	}
-
-	/**
-	 * this method returns a random number n such that
-	 * 0.0 <= n <= 1.0
-	 */
-	public static double randomDouble()
-	{
-		Random r = new Random();
-		return r.nextInt(1000) / 1000.0;
-	}
 	
 	/**
 	 * returns a random int value within a given range
